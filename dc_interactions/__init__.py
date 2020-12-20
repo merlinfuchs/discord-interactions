b7c1a0dacf61caaffca274998a40fbf1a765bc68
Merlintor <merlinfuchs2001@gmail.com>
2020-12-20 02:22:24 +0100

    fix version

--- dc_interactions/__init__.py
+++ dc_interactions/__init__.py
@@ -3,4 +3,8 @@ from payloads import *
 from provider import *
 from response import *
 
-__version__ = 0.1
+__title__ = "dc_interactions"
+__version__ = "0.0.1-pre"
+__author__ = "Merlin Fuchs"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 (c) Merlin Fuchs"
